8604d293191000663c5a4c1369f266100e5c9c97
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-22 17:44:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # require libraries/modules here
 
 require 'nokogiri'
@@ -23,4 +24,36 @@ def create_project_hash
 
   projects
 
+=======
+# file: kickstarter_scraper.rb
+ 
+require 'nokogiri'
+require 'pry'
+ 
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+ 
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+ 
+  projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+ 
+  # return the projects hash
+  projects
+>>>>>>> 38cc1694192ae6f5614423de8ba7ac1c46440915
 end
